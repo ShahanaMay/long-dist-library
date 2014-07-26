4c39c2e8def00fafaa59770d3984eff525ea0200
ShahanaMay <shahana@codelation.com>
2014-07-25 20:48:56 -0500

    add new and show views

--- long_distance_library/app/controllers/books_controller.rb
+++ long_distance_library/app/controllers/books_controller.rb
@@ -3,6 +3,26 @@ class BooksController < ActionController::Base
   # Values needed for my static pages. :)
   def index
     @title = "Books"
-    @book = Book.first
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.create(book_params)
+
+    if @book.save
+      redirect_to book_path, notice: "Book was successfully scheduled. Thank you!"
+    else
+      render :new, alert: "Book was not saved"
+    end
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  private
+  
+  def book_params
+    params.require(:title).permit(:author, :series, :isbn, :illustrator, :genre)
   end
 end
